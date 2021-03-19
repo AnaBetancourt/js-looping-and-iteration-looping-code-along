e6ece42378bcc0e58c9262dcd4352f2a271dc657
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-03-19 02:18:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function writeCards( namesArray, event ) {
+function writeCards(namesArray, event) {
     let thankYouCards = []
-    for ( let i = 0; i < namesArray.length; i++ ) {
-      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
     }
     return thankYouCards
   }
   
-  function countDown( startingNumber ) {
-    while ( startingNumber > 0 ) {
-      console.log( startingNumber );
-      startingNumber -= 1;
+  function countDown(number) {
+    while (number > 0) {
+      console.log(number);
+      number -= 1;
     }
-    console.log( startingNumber );
+    console.log(number);
   }
